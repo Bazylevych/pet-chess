765902f7808cc91a14c538d19030afdfabbe6a1b
thew9rst <79503555+Bazylevych@users.noreply.github.com>
2023-05-14 17:49:39 +0300

    CR-1

---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,9 @@
+import CellComponent from "./CellComponent";
+
+export default function BoardComponent() {
+  return (
+    <div className="board">
+      <CellComponent />
+    </div>
+  );
+}
---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,3 @@
+export default function CellComponent() {
+  return <div className="cell black">cell </div>;
+}
