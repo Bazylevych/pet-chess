daa7b4947360f89e80c8e3a25bfd488c572ff395
thew9rst <79503555+Bazylevych@users.noreply.github.com>
2023-05-15 17:01:51 +0300

    CR-1

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -6,6 +6,8 @@ interface CellProps {
 
 export default function CellComponent({ cell }: CellProps): JSX.Element {
   return (
-    <div className={["cell", cell.color].join(" ")}>{/* {cell.figure} */}</div>
+    <div className={["cell", cell.color].join(" ")}>
+      {cell.figure?.logo && <img src={cell.figure.logo} alt="" />}
+    </div>
   );
 }
