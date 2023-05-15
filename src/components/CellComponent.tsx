d4c8dcb31810eb4a16c3df141be1dbe149f44c17
thew9rst <79503555+Bazylevych@users.noreply.github.com>
2023-05-15 18:12:56 +0300

    CR-1

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -2,11 +2,22 @@ import { Cell } from "../models/Cell";
 
 interface CellProps {
   cell: Cell;
+  selected: boolean;
+  click: (cell: Cell) => void;
 }
 
-export default function CellComponent({ cell }: CellProps): JSX.Element {
+export default function CellComponent({
+  cell,
+  selected,
+  click,
+}: CellProps): JSX.Element {
   return (
-    <div className={["cell", cell.color].join(" ")}>
+    <div
+      className={["cell", cell.color, selected ? "selected" : ""].join(" ")}
+      onClick={() => click(cell)}
+      style={{ background: cell.available && cell.figure ? "green" : "" }}
+    >
+      {cell.available && !cell.figure && <div className="available" />}
       {cell.figure?.logo && <img src={cell.figure.logo} alt="" />}
     </div>
   );
